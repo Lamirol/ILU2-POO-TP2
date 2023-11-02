9c1fd7e383ffebeab0ff99df7d769f0d99a6cc68
Lamirol <lucas.dau65@gmail.com>
2023-11-02 17:28:54 +0100

    TP2-7 fait

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,20 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = this.controlAfficherMarche.donnerInfosMarche();
+		StringBuilder chaine = new StringBuilder();
+		if (infosMarche.length == 0) {
+			chaine.append("Le marché est vide, revenez plus tard.");
+		} else {
+			chaine.append(nomAcheteur + ", vous trouverez au marché :");
+			for (int indiceInfosMarche = 0; indiceInfosMarche<infosMarche.length; indiceInfosMarche++) {
+				String vendeur = infosMarche[indiceInfosMarche];
+				String quantite = infosMarche[++indiceInfosMarche];
+				String produit = infosMarche[++indiceInfosMarche];
+						
+				chaine.append("\n- " + vendeur + " qui vend " + quantite + " " + produit);
+			}
+		}
+		System.out.println(chaine.toString());
 	}
 }
